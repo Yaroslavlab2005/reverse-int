c0de24b99a9d1b1531c90db9a3e21cc98dd16309
Ярослав <kramsk55555@gmail.com>
2025-07-23 15:53:59 +0500

    fix: add radix and fix prettier issue

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
-};
+function reverse(n) {
+  return parseInt(n.toString().split('').reverse().join(''), 10);
+}
+
+module.exports = reverse;
